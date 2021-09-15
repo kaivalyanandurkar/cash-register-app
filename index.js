f4db62e8f272f26feff3f3ffa8a4af11959a25be
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-15 11:44:57 +0530

    style: changed the table style

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
+const cashGiven = document.querySelector("#cash-give");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noofNotes = document.querySelectorAll(".no-of-notes")
 
+
 const availableNotes = [2000,500,100,20,10,5,1]
 
 
-checkButton.addEventListener("click", function validateBillAndCashAmount(){
+checkButton.addEventListener("click", validateBillAndCashAmount);
+function validateBillAndCashAmount() 
+{
+
+
     hideMessage(); 
+
+
     
      if(billAmount.value>0) {
         if(cashGiven.value>=billAmount.value){
@@ -23,7 +30,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount(){
         showMessage("Invalid Bill Amount")
         
     }
-});
+};
 
 
 function calculateChange(amountToBeReturned){
