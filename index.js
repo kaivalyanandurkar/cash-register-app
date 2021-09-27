9236d82f47b175dfc63e13dce4e2cc51a20f6c4d
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-27 09:00:01 +0530

    fix: fixed an issue where an app was displaying wrong message when bill amount was less than cash given.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-give");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
-const noofNotes = document.querySelectorAll(".no-of-notes")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 
 const availableNotes = [2000,500,100,20,10,5,1]
@@ -15,43 +15,47 @@ function validateBillAndCashAmount()
 
     hideMessage(); 
 
-
-    
-     if(billAmount.value>0) {
-        if(cashGiven.value>=billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-
-            if(cashGiven.value===billAmount.value){
-                showMessage("Bill Paid")
-            }
-
-
-        }else{
-            showMessage("Do you wanna wash plates?");
-
+    const billamount = Number(billAmount.value);
+    const cashamount = Number(cashGiven.value);
+      if (cashamount > 0 && billamount > 0) {
+        if (cashamount > billamount) {
+          const amountToBeReturned = cashamount - billamount;
+          calculateChange(amountToBeReturned);
+        } 
+        if(cashamount === billamount){
+             showMessage("Bill paid");
         }
-    } else{
-        showMessage("Invalid Bill Amount")
+        if(cashamount < billamount){
+            showMessage("Do you wanna wash plates?");
+            
+        }
+      } else if(billamount < 0 || billamount === 0){
+        console.log("invalid");
+        showMessage(" Invalid input.");
+     
+      }else {
+        console.log("invalid");
+        showMessage(" Invalid input.");
         
-    }
-};
+      }
+    };
+        
 
 
 function calculateChange(amountToBeReturned){
     for (let i =0; i<availableNotes.length; i++){
-        const numberofNotes = Math.trunc(amountToBeReturned / availableNotes[i]); 
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]); 
         amountToBeReturned = amountToBeReturned % availableNotes[i];
-        noofNotes[i].innerText = numberofNotes;
+        noOfNotes[i].innerText = numberOfNotes;
     }
 
 }
 
 function hideMessage(){
     message.style.display = "none";  
-}
+};
 
 function showMessage(msg){
         message.style.display = "block";
         message.innerText = msg;
-}
+};
